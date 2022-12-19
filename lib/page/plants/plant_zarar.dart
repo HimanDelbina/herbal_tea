174be5a6eee8c4593136e8b738a39bc4cfcf879f
unknown <himandelbina@gmail.com>
2022-12-20 00:30:25 +0330

    create expanstion tile for khavas plant and zarar plant

--- lib/page/plants/plant_zarar.dart
+++ lib/page/plants/plant_zarar.dart
@@ -26,8 +26,8 @@ class _PlantZararState extends State<PlantZarar> {
               : ListView.builder(
                   itemCount: widget.data.length,
                   itemBuilder: (context, index) {
-                    return item_show(
-                        widget.data[index].name, (index + 1).toString());
+                    return item_show(widget.data[index].name,
+                        widget.data[index].description, index + 1);
                   },
                 ),
         ),
@@ -35,21 +35,26 @@ class _PlantZararState extends State<PlantZarar> {
     );
   }
 
-  Widget item_show(String data, String number) {
+  Widget item_show(String title, String description, int index) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-          child: Row(
-            children: [
-              Text(number, style: const TextStyle(color: Colors.redAccent)),
-              const SizedBox(width: 5.0),
-              const Text(" : "),
-              Expanded(child: Text(data, softWrap: true)),
-            ],
-          ),
-        ),
+      child: ExpansionTile(
+        textColor: Colors.red,
+        iconColor: Colors.red,
+        backgroundColor: Colors.red.withOpacity(0.1),
+        controlAffinity: ListTileControlAffinity.trailing,
+        initiallyExpanded: false,
+        title: Text(title,
+            style:
+                const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold)),
+        leading:
+            Text(index.toString(), style: const TextStyle(color: Colors.red)),
+        children: [
+          ListTile(
+              title: Text(description,
+                  style: const TextStyle(fontSize: 14.0),
+                  textAlign: TextAlign.justify))
+        ],
       ),
     );
   }
